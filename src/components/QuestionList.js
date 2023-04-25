ac378ac2d0a3a6ad41029cc6da5caaff43526696
mary_waiyego <waiyegomary55@gmail.com>
2023-04-25 18:10:52 +0300

    get api

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 
 function QuestionList() {
+  const [questionData, setQuestionData] = useState(null)
+  useEffect(() => {
+    const pullData = async() =>{
+      const responseData = await axios.get('http://localhost:4000/questions')
+      setQuestionData(responseData.data)
+    }
+    pullData();
+  },[])  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {
+        questionData?.map((quiz) =>{
+          return(
+            <div>
+            <ul>{quiz.id}</ul>
+            <ul>{quiz.prompt}</ul>
+              <ul>{quiz.answers.toString()}</ul>
+            </div>
+          )
+        })
+      }
     </section>
   );
 }
